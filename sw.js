30b7aec3b2188f1616b28ac46c70f9e00c134483
Bernio <jendabernard@gmail.com>
2025-10-11 11:12:32 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    - stale-while-revalidate pro data/songs.json a /songs/*
    - auto-update: skipWaiting + message handler
 */
-const VERSION = '2025-10-08-23';
+const VERSION = '2025-10-08-24';
 const CACHE_STATIC  = `zpj-static-${VERSION}`;
 const CACHE_DYNAMIC = `zpj-dyn-${VERSION}`;
 
